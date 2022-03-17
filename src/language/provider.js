d52fee9f326d9f0cef1cd6aea315e3570c37e668
Liam Barry Allan <liam@worksofbarry.com>
2022-03-16 23:36:25 -0400

    SQL select content assist

--- src/language/provider.js
+++ src/language/provider.js
@@ -1,14 +1,41 @@
 const vscode = require(`vscode`);
 
+const { Parser } = require(`node-sql-parser`);
+
 const Store = require(`./store`);
+
+/** @type {{[path: string]: object}} */
+const workingAst = {}
 
 /**
  * 
  * @param {vscode.ExtensionContext} context 
  */
 exports.initialise = async (context) => {
+  
   context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument(async (editor) => {
+      const document = editor.document;
+      const text = document.getText();
+
+      if (text.endsWith(`.`)) return;
+
+      if (document.languageId === `sql`) {
+        const parser = new Parser();
+        try {
+          const sqlAst = parser.astify(document.getText(), {
+            database: `DB2`,
+          });
+
+          if (sqlAst) workingAst[document.uri.path] = sqlAst;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }),
+
     vscode.languages.registerCompletionItemProvider({language: `sql` }, {
+      // @ts-ignore
       provideCompletionItems: async (document, position) => {
         ///** @type vscode.CompletionItem[] */
         const items = [];
@@ -21,8 +48,59 @@ exports.initialise = async (context) => {
           items.push(item);
         });
 
+        const ast = workingAst[document.uri.path];
+        if (ast) {
+          if (ast.from && ast.from.length > 0) {
+            ast.from.forEach(definedAs => {
+              const item = new vscode.CompletionItem(definedAs.as || definedAs.table, vscode.CompletionItemKind.Struct);
+              item.detail = `${definedAs.db}.${definedAs.table}`;
+              items.push(item);
+            });
+          }
+        };
+
         return items;
       }
     })
-  )
+  ),
+
+  vscode.languages.registerCompletionItemProvider({language: `sql` }, {
+    // @ts-ignore
+    provideCompletionItems: async (document, position) => {
+      ///** @type vscode.CompletionItem[] */
+      const items = [];
+
+      const ast = workingAst[document.uri.path];
+      if (ast) {
+        if (ast.from && ast.from.length > 0) {
+          const currentPosition = new vscode.Position(position.line, position.character - 1);
+          const range = document.getWordRangeAtPosition(currentPosition);
+
+          if (range) {
+            const prefix = document.getText(range);
+
+            console.log(prefix);
+
+            const definedAs = 
+              ast.from.find(f => f.as === prefix) ||
+              ast.from.find(f => f.table === prefix);
+
+            if (definedAs) {
+              const columns = await Store.getTable(definedAs.db, definedAs.table);
+
+              columns.forEach(column => {
+                const item = new vscode.CompletionItem(column.COLUMN_NAME.toLowerCase(), vscode.CompletionItemKind.Field);
+                item.insertText = new vscode.SnippetString(column.COLUMN_NAME.toLowerCase());
+                item.detail = column.DATA_TYPE;
+                item.documentation = new vscode.MarkdownString(`${column.COLUMN_TEXT} (\`${definedAs.db}.${definedAs.table}\`)`);
+                items.push(item);
+              });
+            }
+          }
+        }
+      }
+      
+      return items;
+    }
+  }, `.`)
 }
