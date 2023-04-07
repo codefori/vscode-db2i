7bac5c7a903f3e551863da37a014fcf8c08c9379
Liam Allan <mrliamallan@live.co.uk>
2023-04-07 09:53:56 -0400

    Initial work for TS
    Signed-off-by: Liam Allan <mrliamallan@live.co.uk>

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require(`vscode`);
+import vscode from "vscode"
 const schemaBrowser = require(`./views/schemaBrowser`);
 
 const JSONServices = require(`./language/json`);
@@ -9,10 +9,7 @@ const resultsProvider = require(`./language/results`);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+export function activate(context: vscode.ExtensionContext) {
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
@@ -30,9 +27,4 @@ function activate(context) {
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-  activate,
-  deactivate
-}
+export function deactivate() {}
