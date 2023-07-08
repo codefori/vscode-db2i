e7dc68164ca4d72357b38879f4102bb09887aaec
worksofliam <mrliamallan@live.co.uk>
2023-07-08 17:59:28 -0400

    Start of statement type

--- src/language/statement.ts
+++ src/language/statement.ts
@@ -1,7 +1,35 @@
 import { IRange, Token } from "./types";
 
+export enum StatementType {
+	Unknown,
+	Create,
+	Select,
+	Update,
+	Delete,
+	Drop
+}
+
+const StatementTypeWord = {
+	'CREATE': StatementType.Create,
+	'SELECT': StatementType.Select,
+	'WITH': StatementType.Select,
+	'UPDATE': StatementType.Update,
+	'DELETE': StatementType.Delete,
+	'DROP': StatementType.Drop
+};
+
 export default class Statement {
-  constructor(public tokens: Token[], public range: IRange) {}
+	private type: StatementType = StatementType.Unknown;
+
+  constructor(public tokens: Token[], public range: IRange) {
+		const first = tokens[0];
+
+		if (first && first.type === `word`) {
+			const wordValue = first.value?.toUpperCase();
+
+			this.type = StatementTypeWord[wordValue];
+		}
+	}
 
 	getTokenByOffset(offset: number) {
 		const blockSearch = (tokens: Token[]): Token|undefined => {
