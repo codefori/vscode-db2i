c6ad704623b75142b0256be1ae9db340d6f8777b
worksofliam <mrliamallan@live.co.uk>
2023-07-10 15:32:40 -0400

    Initial example for token API

--- src/language/index.ts
+++ src/language/index.ts
@@ -1,4 +1,6 @@
 import { CompletionItem, CompletionItemKind, ExtensionContext, languages } from "vscode";
+import Document from "./sql/document";
+import Database from "../database/schemas";
 
 export function languageInit() {
   let functionality = [];
@@ -8,12 +10,32 @@ export function languageInit() {
       `sql`,
       {
         provideCompletionItems(document, position, token, context) {
+          const trigger = context.triggerCharacter;
+          const content = document.getText();
+          const offset = document.offsetAt(position);
+
+          const sqlDoc = new Document(content);
+
+          const currentStatement = sqlDoc.getStatementByOffset(offset);
+          const objectRefs = currentStatement.getObjectReferences();
+
           let list: CompletionItem[] = [];
 
-          const newItem = new CompletionItem(`Hello world`, CompletionItemKind.Keyword);
-          newItem.detail = `This is a sample CompletionItem`;
+          if (trigger === `.`) {
+            const currentRef = currentStatement.getReferenceByOffset(offset);
+            console.log(currentRef);
 
-          list.push(newItem);
+          } else {
+
+            objectRefs.forEach(ref => {
+              if (ref.alias) {
+                list.push(new CompletionItem(ref.alias, CompletionItemKind.File));
+              } else
+              if (ref.object.name) {
+                list.push(new CompletionItem(ref.object.name, CompletionItemKind.File));
+              }
+            });
+          }
 
           return list;
         },
