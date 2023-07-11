e1c5db47b2b31998ca78c85985981adb37c2afd7
worksofliam <mrliamallan@live.co.uk>
2023-07-11 15:12:57 -0400

    Enable test cases for multiple systems

--- src/testing/database.ts
+++ src/testing/database.ts
@@ -1,11 +1,10 @@
 import assert from "assert";
 import { TestSuite } from ".";
 import { JobManager } from "../config";
-import { ServerComponent } from "../connection/serverComponent";
-import { JobStatus } from "../connection/sqlJob";
 import Database from "../database/schemas";
 import Statement from "../database/statement";
 import Callable from "../database/callable";
+import { getInstance } from "../base";
 
 const systemLibrary = `sample`;
 const sqlSchema = `"TestDelimiters"`;
@@ -13,7 +12,51 @@ const sqlSchema = `"TestDelimiters"`;
 export const DatabaseSuite: TestSuite = {
   name: `Database object query tests`,
   tests: [
-    {name: `DelimName tests`, test: async () => {
+    {name: `Create environment`, test: async () => {
+      try {
+        await JobManager.runSQL(`call qsys.create_sql_sample('SAMPLE')`);
+      } catch (e) {
+        console.log(`Probably exists.`);
+      } finally {};
+
+      try {
+        const testDelimStatements = [
+          `DROP SCHEMA "TestDelimiters";`,
+          ``,
+          `CREATE SCHEMA "TestDelimiters";`,
+          `  `,
+          `CREATE TYPE "TestDelimiters"."Delimited Type" AS BIGINT;`,
+          `  `,
+          `CREATE TABLE "TestDelimiters"."Delimited Table" ("Delimited Column" INTEGER DEFAULT NULL, `,
+          `CONSTRAINT "TestDelimiters"."Delimited Key" PRIMARY KEY ("Delimited Column"));`,
+          ``,
+          `CREATE INDEX "TestDelimiters"."Delimited Index" ON "TestDelimiters"."Delimited Table" ("Delimited Column" ASC);`,
+          ``,
+          `CREATE TRIGGER "TestDelimiters"."Delimited Trigger" AFTER INSERT ON "TestDelimiters"."Delimited Table" `,
+          `FOR EACH ROW MODE DB2SQL BEGIN DECLARE X INT; END;`,
+          ``,
+          `CREATE VIEW "TestDelimiters"."Delimited View" ("Delimited Column") AS SELECT * FROM "TestDelimiters"."Delimited Table";`,
+          ``,
+          `CREATE ALIAS "TestDelimiters"."Delimited Alias" FOR "TestDelimiters"."Delimited Table";`,
+          ``,
+          `CREATE FUNCTION "TestDelimiters"."Delimited Function" ("Delimited Parameter" INTEGER) `,
+          `RETURNS INTEGER LANGUAGE SQL BEGIN RETURN "Delimited Parameter"; END;`,
+          ``,
+          `CREATE PROCEDURE "TestDelimiters"."Delimited Procedure" (INOUT "Delimited Parameter" INTEGER) `,
+          `LANGUAGE SQL BEGIN SET "Delimited Parameter" = 13; END;`,
+          ``,
+          `CREATE VARIABLE "TestDelimiters"."Delimited Global Variable" BIGINT;`,
+        ]
+        
+        // Server component doesn't support running multiple queries in one request
+        await getInstance().getContent().runSQL(testDelimStatements.join(`\n`));
+      } catch (e) {
+        console.log(`Possible fail`);
+      
+      } finally {};
+    }},
+
+    {name: `Delim name tests`, test: async () => {
       const systemDelim = Statement.delimName(systemLibrary);
       assert.strictEqual(systemDelim, `SAMPLE`);
 
