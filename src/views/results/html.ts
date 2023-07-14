4f6bc26800001a6230b4f1efa990c675427aa4d3
worksofliam <mrliamallan@live.co.uk>
2023-07-14 10:24:19 -0400

    Simple fix to rendered values

--- src/views/results/html.ts
+++ src/views/results/html.ts
@@ -162,7 +162,7 @@ export function generateScroller(basicSelect: string, isCL: boolean): string {
                 var newCell = newRow.insertCell();
 
                 // Append a text node to the cell
-                var newText = document.createTextNode(row[columnName] || 'null');
+                var newText = document.createTextNode(row[columnName] === undefined ? 'null' : row[columnName]);
                 newCell.appendChild(newText);
               }
             }
