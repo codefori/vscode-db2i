c1654b79de91db9377695ec1e4d0c73cfae93ce2
worksofliam <mrliamallan@live.co.uk>
2023-07-14 15:52:55 -0400

    Markdown tooltip

--- src/views/jobManager/statusBar.ts
+++ src/views/jobManager/statusBar.ts
@@ -1,30 +1,44 @@
-import { StatusBarAlignment, ThemeColor, window } from "vscode";
+import { MarkdownString, StatusBarAlignment, ThemeColor, languages, window } from "vscode";
 import { ServerComponent } from "../../connection/serverComponent";
 import { SQLJobManager } from "../../connection/manager";
 import { JobManager } from "../../config";
+
+const statusItem = languages.createLanguageStatusItem(`sqlStatus`, {language: `sql`});
 
 const item = window.createStatusBarItem(`sqlJob`, StatusBarAlignment.Right);
 
 export async function updateStatusBar() {
   if (ServerComponent.isInstalled()) {
     const selected = JobManager.getSelection();
+
+    let toolTipItems = [];
+
     if (selected) {
-      item.tooltip = `Active SQL job`;
       item.text = `$(database) ${selected.name}`;
 
       if (selected.job.options["transaction isolation"] !== `none`) {
         const pendingsTracts = await selected.job.getPendingTransactions();
         if (pendingsTracts > 0) {
           item.backgroundColor = new ThemeColor('statusBarItem.warningBackground');
-          item.tooltip = `${pendingsTracts} pending change${pendingsTracts !== 1 ? `s` : ``}`;
           item.text = `$(pencil) ${selected.name}`;
+
+          toolTipItems.push(`${pendingsTracts} pending change${pendingsTracts !== 1 ? `s` : ``}. [Commit](command:do-commit)`);
         } else {
           item.backgroundColor = undefined;
         }
       }
+
+      toolTipItems.push(`[View Job Log](command:vscode-db2i.jobManager.viewJobLog)`);
+      toolTipItems.push(`[Edit Connection Settings](command:vscode-db2i.jobManager.editJobProps)`);
     } else {
       item.text = `$(database) No job active`;
+      toolTipItems.push(`[Start Job](command:vscode-db2i.jobManager.newJob)`);
     }
+    
+    const toolTip = new MarkdownString(toolTipItems.join(`\n\n---\n\n`), true);
+    toolTip.isTrusted = true;
+    
+    item.tooltip = toolTip;
 
     item.show();
   } else {
