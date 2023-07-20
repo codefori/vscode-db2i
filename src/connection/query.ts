679d1c981c70c74d307f9543fbd5665b4e7adb3c
worksofliam <mrliamallan@live.co.uk>
2023-07-19 22:55:10 -0400

    Add concept of auto closing cursors

--- src/connection/query.ts
+++ src/connection/query.ts
@@ -17,23 +17,46 @@ export class Query<T> {
   private isCLCommand: boolean;
   private state: QueryState = QueryState.NOT_YET_RUN;
 
-  constructor(private job: SQLJob, query: string, opts: QueryOptions = {isClCommand: false, parameters: undefined}) {
+  public autoClose: boolean | undefined;
+
+  constructor(private job: SQLJob, query: string, opts: QueryOptions = { isClCommand: false, parameters: [], autoClose: false }) {
     this.job = job;
     this.isPrepared = (undefined !== opts.parameters);
     this.parameters = opts.parameters;
     this.sql = query;
     this.isCLCommand = opts.isClCommand;
+    this.autoClose = opts.autoClose;
+
     Query.globalQueryList.push(this);
   }
+
   public static byId(id: string) {
     return (undefined === id || '' === id) ? undefined : Query.globalQueryList.find(query => query.correlationId === id);
   }
+
+  public static async cleanup() {
+    let closePromises = [];
+
+    // First, let's check to see if we should also cleanup
+    // any cursors that remain open, and we've been told to close
+    for (const query of this.globalQueryList) {
+      if (query.autoClose) {
+        closePromises.push(query.close())
+      }
+    };
+
+    await Promise.all(closePromises);
+
+    // Automatically remove any queries done and dusted. They're useless.
+    this.globalQueryList = this.globalQueryList.filter(q => q.getState() !== QueryState.RUN_DONE);
+  }
+
   public async run(rowsToFetch: number = this.rowsToFetch): Promise<QueryResult<T>> {
     switch (this.state) {
-    case QueryState.RUN_MORE_DATA_AVAILABLE:
-      throw new Error('Statement has already been run');
-    case QueryState.RUN_DONE:
-      throw new Error('Statement has already been fully run');
+      case QueryState.RUN_MORE_DATA_AVAILABLE:
+        throw new Error('Statement has already been run');
+      case QueryState.RUN_DONE:
+        throw new Error('Statement has already been fully run');
     }
     let queryObject;
     if (this.isCLCommand) {
@@ -55,7 +78,7 @@ export class Query<T> {
     let result = await this.job.send(JSON.stringify(queryObject));
     let queryResult: QueryResult<T> = JSON.parse(result);
 
-    this.state = queryResult.is_done? QueryState.RUN_DONE: QueryState.RUN_MORE_DATA_AVAILABLE;
+    this.state = queryResult.is_done ? QueryState.RUN_DONE : QueryState.RUN_MORE_DATA_AVAILABLE;
 
     if (queryResult.success !== true && !this.isCLCommand) {
       this.state = QueryState.ERROR;
@@ -64,13 +87,14 @@ export class Query<T> {
     this.correlationId = queryResult.id;
     return queryResult;
   }
+
   public async fetchMore(rowsToFetch: number = this.rowsToFetch): Promise<QueryResult<T>> {
     //TODO: verify that the SQL job hasn't changed
     switch (this.state) {
-    case QueryState.NOT_YET_RUN:
-      throw new Error('Statement has not yet been run');
-    case QueryState.RUN_DONE:
-      throw new Error('Statement has already been fully run');
+      case QueryState.NOT_YET_RUN:
+        throw new Error('Statement has not yet been run');
+      case QueryState.RUN_DONE:
+        throw new Error('Statement has already been fully run');
     }
     let queryObject = {
       id: SQLJob.getNewUniqueRequestId(`fetchMore`),
@@ -84,7 +108,7 @@ export class Query<T> {
     let result = await this.job.send(JSON.stringify(queryObject));
 
     let queryResult: QueryResult<T> = JSON.parse(result);
-    this.state = queryResult.is_done? QueryState.RUN_DONE: QueryState.RUN_MORE_DATA_AVAILABLE;
+    this.state = queryResult.is_done ? QueryState.RUN_DONE : QueryState.RUN_MORE_DATA_AVAILABLE;
 
     if (queryResult.success !== true) {
       this.state = QueryState.ERROR;
@@ -92,12 +116,24 @@ export class Query<T> {
     }
     return queryResult;
   }
+
   public async close() {
+    // TODO: close the cursor
     this.state = QueryState.RUN_DONE;
+
+    let queryObject = {
+      id: SQLJob.getNewUniqueRequestId(`sqlclose`),
+      cont_id: this.correlationId,
+      type: `sqlclose`,
+    };
+
+    return this.job.send(JSON.stringify(queryObject));
   }
+
   public getId(): string {
     return this.correlationId;
   }
+
   public getState(): QueryState {
     return this.state;
   }
