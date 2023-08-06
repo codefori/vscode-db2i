78b85748feb53939bd2d8f69050ced91cd94f3d3
worksofliam <mrliamallan@live.co.uk>
2023-08-06 19:42:13 -0400

    Add additional checks where subselect is possible

--- src/language/sql/statement.ts
+++ src/language/sql/statement.ts
@@ -101,6 +101,8 @@ export default class Statement {
 		const doAdd = (ref?: ObjectRef) => {
 			if (ref) list.push(ref);
 		}
+		
+		let inFromClause = false;
 
 		switch (this.type) {
 			case StatementType.Call:
@@ -130,7 +132,6 @@ export default class Statement {
 			case StatementType.Select:
 			case StatementType.Delete:
 				// SELECT
-				let inFromClause = false;
 				for (let i = 0; i < this.tokens.length; i++) {
 					if (tokenIs(this.tokens[i], `keyword`, `FROM`)) {
 						inFromClause = true;
@@ -185,8 +186,14 @@ export default class Statement {
 							break;
 
 						case `VIEW`:
-							for (let i = postName; i < this.tokens.length; i++) {
-								if (tokenIs(this.tokens[i], `keyword`, `FROM`) || tokenIs(this.tokens[i], `keyword`, `INTO`) || tokenIs(this.tokens[i], `join`)) {
+							for (let i = 0; i < this.tokens.length; i++) {
+								if (tokenIs(this.tokens[i], `keyword`, `FROM`)) {
+									inFromClause = true;
+								} else if (inFromClause && tokenIs(this.tokens[i], `CLAUSE`) || tokenIs(this.tokens[i], `JOIN`) || tokenIs(this.tokens[i], `closebracket`)) {
+									inFromClause = false;
+								}
+			
+								if (tokenIs(this.tokens[i], `keyword`, `FROM`) || tokenIs(this.tokens[i], `keyword`, `INTO`) || tokenIs(this.tokens[i], `join`) || (inFromClause && tokenIs(this.tokens[i], `comma`))) {
 									doAdd(this.getRefAtToken(i+1));
 								}
 							}
