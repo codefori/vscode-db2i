f8485488644f0b183200db029177d291d10ef03a
worksofliam <mrliamallan@live.co.uk>
2023-09-25 12:38:23 -0400

    Either grab specific name or check against specific name

--- src/database/callable.ts
+++ src/database/callable.ts
@@ -12,12 +12,12 @@ export default class Callable {
    */
   static getParms(schema: string, specificName: string): Promise<SQLParm[]> {
     const rowType = `P`; // Parameter
-    const options : QueryOptions = { parameters : [schema, schema, specificName, rowType] };
+    const options : QueryOptions = { parameters : [schema, schema, specificName, specificName, rowType] };
     return JobManager.runSQL<SQLParm>([
       `SELECT * FROM QSYS2.SYSPARMS`,
-      `WHERE SPECIFIC_SCHEMA = ? AND SPECIFIC_NAME = (
+      `WHERE SPECIFIC_SCHEMA = ? AND (SPECIFIC_NAME = (
         select specific_name from qsys2.sysroutines where specific_schema = ? and routine_name = ?
-      ) and ROW_TYPE = ?`,
+      ) or SPECIFIC_NAME = ?) and ROW_TYPE = ?`,
       `ORDER BY ORDINAL_POSITION`
     ].join(` `),
     options);
