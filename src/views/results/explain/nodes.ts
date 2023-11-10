3b7c9b1866257f663c2a3cd050416b558c4fba85
Dave Charron <david.s.charron@gmail.com>
2023-11-10 14:18:26 -0600

    Process string data returned via multiple records

--- src/views/results/explain/nodes.ts
+++ src/views/results/explain/nodes.ts
@@ -43,7 +43,7 @@ export class ExplainTree {
     const nodeId = this.order[index];
     /** @type {any[]} */
     const nodeData = this.flatNodes[nodeId];
-  
+
     let currentNode: ExplainNode = {
       id: nodeId,
       title: ``,
@@ -51,24 +51,55 @@ export class ExplainTree {
       children: [],
       props: []
     };
-  
+
+    // The IFA_CHROUT column is VARCHAR(128), which means data that needs to be returned that exceeds that length must flow over to additional records
+    let longString = ``;
+    let processingLongString = false;
+
+    // TODO: add logic for processing records that follow a CHANGED_ATTRIBUTES_INDICATOR record
+    //       once such a record is encountered, all following records provide new values for
+    //       previously processed attributes, so we need to look back at currentNode.props to find
+    //       the right entry to update with a new value.  Fun!
+
     for (const data of nodeData) {
       // TODO: list of column types
+
+      let nodeValue: any;
+      switch (data.IFA_TYPOUT) {
+        case ValueType.LONG_STRING:
+          // Type indicates a long string, meaning one or more following records continue the value
+          processingLongString = true;
+          longString += data.IFA_CHROUT;
+          continue;
+        case ValueType.NUMERIC:
+          nodeValue = data.IFA_NUMOUT;
+          break;
+        case ValueType.CHARACTER:
+        default:
+          // If we were processing a long string, we've reached the end of the data, so wrap it up, clear the long string mode
+          if (processingLongString) {
+            nodeValue = longString + data.IFA_CHROUT;
+            longString = ``;
+            processingLongString = false;
+            break;
+          }
+          nodeValue = data.IFA_CHROUT;
+          break;
+      }
+
       const nodeDataType = data.IFA_COLTYP;
       const nodeTitle = data.IFA_COLHDG;
-      const nodeValue = data.IFA_TYPOUT === ValueType.NUMERIC ? data.IFA_NUMOUT : data.IFA_CHROUT;
-  
       switch (nodeDataType) {
-      case RecordType.NEW_ICON:
-        currentNode.title = nodeValue;
-        break;
-      case RecordType.CHILD_COUNT:
-        currentNode.childrenNodes = nodeValue;
-        break;
-      case RecordType.CHILD_ICON:
-        // TODO: what do we do with this?
-        break;
-      case RecordType.HEADING:
+        case RecordType.NEW_ICON:
+          currentNode.title = nodeValue;
+          break;
+        case RecordType.CHILD_COUNT:
+          currentNode.childrenNodes = nodeValue;
+          break;
+        case RecordType.CHILD_ICON:
+          // TODO: what do we do with this?
+          break;
+        case RecordType.HEADING:
           // If not the first node, add a blank node before the heading to space things out a bit
           if (currentNode.props.length > 0) {
             currentNode.props.push({
@@ -83,29 +114,31 @@ export class ExplainTree {
             value: ``
           });
           break;
-      default:
-        if (nodeValue || nodeValue === 0) {
-          currentNode.props.push({
-            type: nodeDataType,
-            title: nodeTitle,
-            value: nodeValue
-          });
-        }
+        default:
+          if (nodeValue || nodeValue === 0) {
+            currentNode.props.push({
+              type: nodeDataType,
+              title: nodeTitle,
+              value: nodeValue
+            });
+          }
       }
     }
-  
+
     for (let subIndex = 0; subIndex < currentNode.childrenNodes; subIndex++) {
       this.nextNodeIndex += 1;
       currentNode.children.push(this.handleNode(this.nextNodeIndex));
     }
-  
+
     if (currentNode.childrenNodes !== currentNode.children.length) {
       throw new Error(`This makes no sense.`);
     }
-  
+
     return currentNode;
   }
 }
+
+const DELTA_ATTRIBUTES_INDICATOR = `BEGIN DELTA ATTRIBUTES`;
 
 /**
  * Record type indicators from the type column ( IFA_COLTYP )
