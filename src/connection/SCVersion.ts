67e464000dfc26591e0d4528e0c94dd66c56c000
Dave Charron <david.s.charron@gmail.com>
2024-02-01 10:23:11 -0600

    Bump server version again for VE failure cleanup

--- src/connection/SCVersion.ts
+++ src/connection/SCVersion.ts
@@ -1,3 +1,3 @@
 
-export const SERVER_VERSION_TAG = `v1.4.8`;
-export const SERVER_VERSION_FILE = `codeforiserver-1.4.8.jar`;
+export const SERVER_VERSION_TAG = `v1.4.9`;
+export const SERVER_VERSION_FILE = `codeforiserver-1.4.9.jar`;
