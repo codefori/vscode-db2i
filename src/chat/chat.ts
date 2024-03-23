8c32619a532feb2b9f4863bc0b55de7f58a224de
worksofliam <mrliamallan@live.co.uk>
2024-03-22 21:44:07 -0400

    Change prompt inputs

--- src/chat/chat.ts
+++ src/chat/chat.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { JobManager } from "../config";
+import Statement from "../database/statement";
 
 const CHAT_ID = `vscode-db2i.chat`;
 const LANGUAGE_MODEL_ID = `copilot-gpt-3.5-turbo`;
@@ -8,6 +9,72 @@ interface IDB2ChatResult extends vscode.ChatResult {
   metadata: {
     command: string;
   };
+}
+
+const getDefaultSchema = (): string => {
+  const currentJob = JobManager.getSelection();
+  return currentJob && currentJob.job.options.libraries[0] ? currentJob.job.options.libraries[0] : `QGPL`;
+}
+
+type TableRefs = { [key: string]: TableColumn[] };
+
+async function findPossibleTables(schema: string, words: string[]) {
+  const validWords = words.filter(item => item.length > 2 && !item.includes(`'`)).map(item => `'${Statement.delimName(item, true)}'`);
+
+  const objectFindStatement = [
+    `SELECT `,
+    `  column.TABLE_NAME,`,
+    `  column.COLUMN_NAME,`,
+    `  key.CONSTRAINT_NAME,`,
+    `  column.DATA_TYPE, `,
+    `  column.CHARACTER_MAXIMUM_LENGTH,`,
+    `  column.NUMERIC_SCALE, `,
+    `  column.NUMERIC_PRECISION,`,
+    `  column.IS_NULLABLE, `,
+    // `  column.HAS_DEFAULT, `,
+    // `  column.COLUMN_DEFAULT, `,
+    `  column.COLUMN_TEXT, `,
+    `  column.IS_IDENTITY`,
+    `FROM QSYS2.SYSCOLUMNS2 as column`,
+    `LEFT JOIN QSYS2.syskeycst as key`,
+    `  on `,
+    `    column.table_schema = key.table_schema and`,
+    `    column.table_name = key.table_name and`,
+    `    column.column_name = key.column_name`,
+    `WHERE column.TABLE_SCHEMA = '${getDefaultSchema()}' AND column.TABLE_NAME in (${validWords.join(`, `)})`,
+    `ORDER BY column.ORDINAL_POSITION`,
+  ].join(` `);
+
+  // TODO
+  const result: TableColumn[] = await JobManager.runSQL(objectFindStatement);
+
+  const tables: TableRefs = {};
+
+  for (const row of result) {
+    if (!tables[row.TABLE_NAME]) {
+      tables[row.TABLE_NAME] = [];
+    }
+
+    tables[row.TABLE_NAME].push(row);
+  }
+
+  return tables;
+}
+
+function refsToMarkdown(refs: TableRefs) {
+  let markdown: string[] = [];
+
+  for (const tableName in refs) {
+    markdown.push(`# ${tableName}`, ``);
+
+    for (const column of refs[tableName]) {
+      markdown.push(`| name: ${column.COLUMN_NAME} | type: ${column.DATA_TYPE} | nullable: ${column.IS_NULLABLE} | identity: ${column.IS_IDENTITY} | text: ${column.COLUMN_TEXT} | constraint: ${column.CONSTRAINT_NAME} |`);
+    }
+
+    markdown.push(``);
+  }
+
+  return markdown.join(`\n`);
 }
 
 export function activateChat(context: vscode.ExtensionContext) {
@@ -21,12 +88,22 @@ export function activateChat(context: vscode.ExtensionContext) {
 
     let messages: (vscode.LanguageModelChatSystemMessage | vscode.LanguageModelChatUserMessage)[];
 
+    const usingSchema = getDefaultSchema();
+
     switch (request.command) {
       case `build`:
-        stream.progress(`Querying database for information...`);
+        stream.progress(`Getting information from ${Statement.prettyName(usingSchema)}...`);
+        const refs = await findPossibleTables(usingSchema, request.prompt.split(` `));
+        stream.progress(`Building response...`);
         messages = [
           new vscode.LanguageModelChatSystemMessage(
-            `You are a an IBM i savant speciallizing in database features in Db2 for i. Your job is to help developers write and debug their SQL along with offering SQL programming advice. Help the developer write an SQL statement based on the prompt information. Always include code examples where is makes sense.`
+            `You are a an IBM i savant speciallizing in database features in Db2 for i. Your job is to help developers write and debug their SQL along with offering SQL programming advice.`
+          ),
+          new vscode.LanguageModelChatSystemMessage(
+            `Give the developer an SQL statement or information based on the prompt and following table references. Always include code examples where is makes sense.`
+          ),
+          new vscode.LanguageModelChatSystemMessage(
+            `Here are the table references:\n${refsToMarkdown(refs)}`
           ),
           new vscode.LanguageModelChatUserMessage(request.prompt),
         ];
@@ -41,11 +118,12 @@ export function activateChat(context: vscode.ExtensionContext) {
         console.log(`summarize the following data in a readable paragraph: ${data}`)
         messages = [
           new vscode.LanguageModelChatSystemMessage(
-            `You are a an IBM i savant speciallizing in database features in Db2 for i. Your job is to help developers write and debug their SQL along with offering SQL programming advice. Help the developer write an SQL statement based on the prompt information. Always include code examples where is makes sense.`
+            `You are a an IBM i savant speciallizing in database features in Db2 for i. Please provide a summary of the current IBM i system state based on the developer requirement.`
           ),
-          new vscode.LanguageModelChatUserMessage(
-            `summarize the following data in a readable paragraph: ${data}`
+          new vscode.LanguageModelChatSystemMessage(
+            `Here is the current IBM i state: ${data}`
           ),
+          new vscode.LanguageModelChatUserMessage(request.prompt),
         ];
   
         await streamModelResponse(messages, stream, token);
