17da100b8a9f32072fb7f1a06fc185c26bee215b
worksofliam <mrliamallan@live.co.uk>
2024-08-08 15:05:35 -0400

    Support for data areas
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/views/variables/index.ts
+++ src/views/variables/index.ts
@@ -5,8 +5,12 @@ import { SQLExample, Examples, ServiceInfoLabel } from "../examples";
 import { SQLObject } from "../schemaBrowser";
 import ColumnTreeItem from "../types/ColumnTreeItem";
 import { JobManager } from "../../config";
+import Statement from "../../database/statement";
+
+type VariableType = "sql"|"dataarea";
 
 interface Variable {
+  type: VariableType,
   label: string;
   statement: string;
 }
@@ -19,17 +23,32 @@ export class Variables implements TreeDataProvider<any> {
 
   constructor(context: ExtensionContext) {
     context.subscriptions.push(
-      commands.registerCommand(`vscode-db2i.variables.add`, async () => {
-        // TODO: support chosen parameter
-        const userInput = await window.showInputBox({
-          placeHolder: `schema.variableName`,
-          title: `Enter an existing variable name`
-        });
+      commands.registerCommand(`vscode-db2i.variables.add`, async (object?: SQLObject) => {
+        let fetchStatement;
+        
+        if (object && object.type === `variable`) {
+          fetchStatement = Statement.delimName(object.schema) + `.` + Statement.delimName(object.name);
+        }
 
-        if (userInput) {
+        if (!fetchStatement) {
+          // TODO: support chosen parameter
+          fetchStatement = await window.showInputBox({
+            placeHolder: `schema.variableName`,
+            title: `Enter an existing variable name`
+          });
+        }
+
+        if (fetchStatement) {
+          let type: VariableType = `sql`;
+
+          if (fetchStatement.length < 21 && fetchStatement.includes(`/`)) {
+            type = `dataarea`;
+          }
+
           this.variables.push({
-            label: userInput,
-            statement: userInput
+            type,
+            label: fetchStatement,
+            statement: fetchStatement
           });
 
           this.refresh();
@@ -57,12 +76,12 @@ export class Variables implements TreeDataProvider<any> {
     return element;
   }
 
-  async getChildren(element?: VariableTreeItem): Promise<VariableTreeItem[]> {
+  async getChildren(element?: VariableTreeItem): Promise<TreeItem[]> {
     if (this.variables.length === 0) return [];
 
     const sql = this.buildStatement();
 
-    let variableResults: VariableTreeItem[];
+    let variableResults: TreeItem[];
     try {
       const results = await JobManager.runSQL(sql);
 
@@ -72,31 +91,42 @@ export class Variables implements TreeDataProvider<any> {
 
         variableResults = labels.map((label, index) => {
           return new VariableTreeItem(label, {
-            value: firstRow[label]
+            value: String(firstRow[label])
           });
         });
       } else {
         // TODO: welcome page 
       }
     } catch (e) {
-      //TODO: better error handling
-      variableResults = this.variables.map(variable => 
+      variableResults = [new VariableTreeItemError(e.message)];
+      variableResults.push(...this.variables.map(variable => 
         new VariableTreeItem(variable.label, {
-          error: e.message.includes(variable.label) ? e.message : undefined
+          error: true
         })
-      );
+      ));
     }
 
     return variableResults;
   }
 
   private buildStatement(): string {
-    return `select ${this.variables.map(variable => `(${variable.statement}) as "${variable.label}"`).join(`, `)} from sysibm.sysdummy1`;
+    return `select ${this.variables.map(variable => {
+      switch (variable.type) {
+        case `sql`:
+          return `${variable.statement} as "${variable.label}"`;
+        case `dataarea`:
+          const parts = variable.statement.split(`/`);
+          if (parts.length !== 2) {
+            return `'invalid name' as "${variable.label}"`;
+          }
+          return `(select DATA_AREA_VALUE from table(qsys2.data_area_info(data_area_library => '${parts[0].toUpperCase()}', data_area_name => '${parts[1].toUpperCase()}'))) as "${variable.label}"`
+      }
+    }).join(`, `)} from sysibm.sysdummy1`;
   }
 }
 
 class VariableTreeItem extends TreeItem {
-  constructor(variable: string, detail: {error?: string, value?: string}) {
+  constructor(variable: string, detail: {error?: boolean, value?: string}) {
     super(variable, TreeItemCollapsibleState.None);
     this.contextValue = `sqlVarValue`;
 
@@ -115,11 +145,18 @@ class VariableTreeItem extends TreeItem {
 
     if (detail.error) {
       this.iconPath = new ThemeIcon(`error`);
-      this.description = detail.error;
     }
 
     if (!detail.value && !detail.error) {
       this.iconPath = new ThemeIcon(`question`);
     }
+  }
+}
+
+class VariableTreeItemError extends TreeItem {
+  constructor(label: string) {
+    super(label, TreeItemCollapsibleState.None);
+    this.contextValue = `sqlVarValueError`;
+    this.iconPath = new ThemeIcon(`warning`);
   }
 }
