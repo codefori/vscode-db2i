72e7ddb3175e0098136387061bfa42886ae34ecd
worksofliam <mrliamallan@live.co.uk>
2024-12-03 14:41:10 -0500

    Minor cleanup
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,4 +1,4 @@
-import vscode from "vscode";
+import * as vscode from "vscode";
 import { env } from "process";
 import { TestSuitesTreeProvider } from "./testCasesTree";
 import { getInstance } from "../base";
