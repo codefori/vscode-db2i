437988885c206d17d4aeb9da111544d98898c37c
Adam Shedivy <ajshedivyaj@gmail.com>
2024-12-16 17:10:32 -0600

    add schema context item to menu

--- src/aiProviders/continue/listTablesContextProvider.ts
+++ src/aiProviders/continue/listTablesContextProvider.ts
@@ -4,6 +4,7 @@ import { JobInfo } from "../../connection/manager";
 import { JobManager } from "../../config";
 import { isContinueActive } from "./continueContextProvider";
 import { findPossibleTables } from "../context";
+import Statement from "../../database/statement";
 
 const listDb2Table: ContextProviderDescription = {
   title: "list Db2i Tables",
@@ -27,8 +28,8 @@ class ListDb2iTables implements IContextProvider {
     return currentJob?.job.options.libraries[0] || "QGPL";
   }
 
-  async getTables() {
-    const schema = this.getDefaultSchema().toUpperCase();
+  async getTables(curSchema: string) {
+    const schema = Statement.delimName(curSchema, true);
     const sql = `
       SELECT TABLE_NAME, TABLE_SCHEMA
       FROM QSYS2.SYSTABLES
@@ -39,12 +40,31 @@ class ListDb2iTables implements IContextProvider {
 
     const result = await JobManager.runSQL(sql);
     return result;
+  }
 
+  async getColumnInfoForAllTables(schema: string) {
+    const sql = `
+      SELECT COLUMN_NAME, TABLE_NAME, DATA_TYPE
+      FROM QSYS2.SYSCOLUMNS
+      WHERE TABLE_SCHEMA = '${Statement.delimName(schema)}'
+    `;
+
+    const result = await JobManager.runSQL(sql);
+    return result;
 
   }
 
   async getContextItems(query: string, extras: ContextProviderExtras): Promise<ContextItem[]> {
-    const tableInfo = await findPossibleTables(null, this.getDefaultSchema(), query.split(` `));
+    const schema = this.getDefaultSchema();
+    if (query.toUpperCase() === schema.toUpperCase()) {
+      const tableInfo = await this.getColumnInfoForAllTables(schema);
+      return [{
+        name: `Info for all tables in ${schema}`,
+        content: JSON.stringify(tableInfo),
+        description: "table metadata"
+      }]
+    }
+    const tableInfo = await findPossibleTables(null, schema, query.split(` `));
     return [{
       name: `${query}`,
       content: JSON.stringify(tableInfo),
@@ -52,17 +72,24 @@ class ListDb2iTables implements IContextProvider {
 
     }]
   }
+  
   async loadSubmenuItems(args: LoadSubmenuItemsArgs): Promise<ContextSubmenuItem[]> {
-    const tables: any[] = await this.getTables();
+    const schema = this.getDefaultSchema();
+    const tables: any[] = await this.getTables(schema);
+    
+    const schemaSubmenuItem: ContextSubmenuItem = {
+      id: schema,
+      title: schema,
+      description: `All table info in schema: ${schema}`
+    };
 
-    return tables.map((table) => {
-      return {
-        id: table.TABLE_NAME,
-        title: table.TABLE_NAME,
-        description: `${table.TABLE_SCHEMA}-${table.TABLE_NAME}`,
-      }
-    })
+    const tableSubmenuItems: ContextSubmenuItem[] = tables.map((table) => ({
+      id: table.TABLE_NAME,
+      title: table.TABLE_NAME,
+      description: `${table.TABLE_SCHEMA}-${table.TABLE_NAME}`,
+    }));
 
+    return [schemaSubmenuItem, ...tableSubmenuItems];
   }
   
 }
