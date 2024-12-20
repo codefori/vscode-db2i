9c3d98dce7c36655a032554da3ca6807164d7136
worksofliam <mrliamallan@live.co.uk>
2024-12-20 09:56:58 -0500

    Add configuration option to check SQL syntax on file open
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/language/providers/problemProvider.ts
+++ src/language/providers/problemProvider.ts
@@ -1,4 +1,4 @@
-import { commands, CompletionItemKind, Diagnostic, DiagnosticSeverity, languages, Range, Uri, window, workspace } from "vscode";
+import { commands, CompletionItemKind, Diagnostic, DiagnosticSeverity, languages, Range, TextDocument, Uri, window, workspace } from "vscode";
 import {
   SQLType,
 } from "../../database/schemas";
@@ -26,7 +26,11 @@ const diagnosticTypeMap: { [key: string]: DiagnosticSeverity } = {
 let currentTimeout: NodeJS.Timeout;
 let sqlDiagnosticCollection = languages.createDiagnosticCollection(`db2i-sql`);
 
-export function getCheckerAutomatically() {
+export function checkOnOpen() {
+  return (Configuration.get<boolean>(`syntax.checkOnOpen`) || false);
+}
+
+export function checkOnChange() {
   return (Configuration.get<boolean>(`syntax.checkAutomatically`) || false);
 }
 
@@ -40,10 +44,6 @@ function shouldShowWarnings() {
 
 const CHECKER_AVAILABLE_CONTEXT = `vscode-db2i.syntax.checkerAvailable`;
 
-function shouldShowError(error: SqlSyntaxError) {
-  return error.type === `error` || (error.type === `warning` && shouldShowWarnings());
-}
-
 export function setCheckerAvailableContext() {
   const available = SQLStatementChecker.get() !== undefined;
   commands.executeCommand(`setContext`, CHECKER_AVAILABLE_CONTEXT, available);
@@ -51,111 +51,90 @@ export function setCheckerAvailableContext() {
 
 export const CHECK_DOCUMENT_COMMAND = `vscode-db2i.syntax.checkDocument`;
 export const checkDocumentDefintion = commands.registerCommand(CHECK_DOCUMENT_COMMAND, async (uri?: Uri) => {
-  const checker = SQLStatementChecker.get();
-  if (checker) {
-    const document = uri ? (await workspace.openTextDocument(uri)) : window.activeTextEditor?.document;
+  const document = uri ? (await workspace.openTextDocument(uri)) : window.activeTextEditor?.document;
 
-    if (document) {
-      const content = document.getText();
-      const sqlDocument = new Document(content);
-
-      const allGroups = sqlDocument.getStatementGroups();
-      let statementRanges: StatementRange[] = [];
-
-      for (const group of allGroups) {
-        const range = getStatementRangeFromGroup(group);
-        if (range) {
-          statementRanges.push(range);
-        }
-      }
-
-      const sqlStatementContents = statementRanges.map(range => content.substring(range[0], range[1]));
-      const syntaxChecked = await checker.checkMultipleStatements(sqlStatementContents);
-
-      if (syntaxChecked) {
-        if (syntaxChecked.length > 0) {
-          let errors: Diagnostic[] = [];
-          for (let i = 0; i < statementRanges.length; i++) {
-            const currentRange = statementRanges[i];
-            const groupError = syntaxChecked[i];
-
-            if (shouldShowError(groupError)) {
-
-              const selectedWord
-                = document.getWordRangeAtPosition(document.positionAt(currentRange[0] + groupError.offset))
-                || new Range(
-                  document.positionAt(currentRange[0] + groupError.offset - 1),
-                  document.positionAt(currentRange[0] + groupError.offset)
-                );
-
-              errors.push({
-                message: `${groupError.text} - ${groupError.sqlstate}`,
-                code: groupError.sqlid,
-                range: selectedWord,
-                severity: diagnosticTypeMap[groupError.type],
-              });
-            }
-          }
-
-          sqlDiagnosticCollection.set(document.uri, errors);
-        }
-      }
-    }
+  if (document) {
+    await validateSqlDocument(document);
   }
 });
 
-export const problemProvider = workspace.onDidChangeTextDocument(e => {
-  const isSql = e.document.languageId === `sql`;
+export const problemProvider = [
+  workspace.onDidOpenTextDocument(e => {
+    const isSql = e.languageId === `sql`;
+    if (isSql && checkOnOpen()) {
+      validateSqlDocument(e);
+    }
+  }),
+
+  workspace.onDidChangeTextDocument(e => {
+    const isSql = e.document.languageId === `sql`;
+    if (isSql && checkOnChange()) {
+      if (currentTimeout) {
+        clearTimeout(currentTimeout);
+      }
+
+      currentTimeout = setTimeout(() => {
+        validateSqlDocument(e.document);
+      }, getCheckerTimeout());
+    }
+  })
+];
+
+async function validateSqlDocument(document: TextDocument) {
   const checker = SQLStatementChecker.get();
-  if (isSql && remoteAssistIsEnabled() && getCheckerAutomatically() && checker) {
-    if (currentTimeout) {
-      clearTimeout(currentTimeout);
+  if (remoteAssistIsEnabled() && checker) {
+    const content = document.getText();
+    const sqlDocument = new Document(content);
+
+    const allGroups = sqlDocument.getStatementGroups();
+    let statementRanges: StatementRange[] = [];
+
+    for (const group of allGroups) {
+      const range = getStatementRangeFromGroup(group);
+      if (range) {
+        statementRanges.push(range);
+      }
     }
 
-    currentTimeout = setTimeout(async () => {
-      const [change] = e.contentChanges;
-      if (change) {
-        const document = e.document;
-        const position = e.contentChanges[0].range.start;
-        const content = document.getText();
-        const offset = document.offsetAt(position);
+    const sqlStatementContents = statementRanges.map(range => content.substring(range[0], range[1]));
+    const syntaxChecked = await checker.checkMultipleStatements(sqlStatementContents);
 
-        const sqlDoc = new Document(content);
-        const currentGroup = sqlDoc.getGroupByOffset(offset);
+    if (syntaxChecked) {
+      if (syntaxChecked.length > 0) {
+        let errors: Diagnostic[] = [];
+        for (let i = 0; i < statementRanges.length; i++) {
+          const currentRange = statementRanges[i];
+          const groupError = syntaxChecked[i];
 
-        if (currentGroup) {
-          const statementRange = getStatementRangeFromGroup(currentGroup);
+          if (shouldShowError(groupError)) {
 
-          if (statementRange) {
-            const statementContents = content.substring(statementRange[0], statementRange[1]);
+            const selectedWord = document.getWordRangeAtPosition(document.positionAt(currentRange[0] + groupError.offset))
+              || new Range(
+                document.positionAt(currentRange[0] + groupError.offset - 1),
+                document.positionAt(currentRange[0] + groupError.offset)
+              );
 
-            const result = await checker.call(statementContents);
-            if (result && shouldShowError(result)) {
-              const selectedWord
-                = document.getWordRangeAtPosition(document.positionAt(statementRange[0] + result.offset))
-                || new Range(
-                  document.positionAt(statementRange[0] + result.offset - 1),
-                  document.positionAt(statementRange[0] + result.offset)
-                );
-
-              sqlDiagnosticCollection.set(document.uri, [{
-                message: `${result.text} - ${result.sqlstate}`,
-                code: result.sqlid,
-                range: selectedWord,
-                severity: diagnosticTypeMap[result.type],
-              }]);
-            } else {
-              sqlDiagnosticCollection.set(document.uri, []);
-            }
+            errors.push({
+              message: `${groupError.text} - ${groupError.sqlstate}`,
+              code: groupError.sqlid,
+              range: selectedWord,
+              severity: diagnosticTypeMap[groupError.type],
+            });
           }
         }
-      }
-    }, getCheckerTimeout());
-  }
-});
 
-function getStatementRangeFromGroup(currentGroup: StatementGroup): StatementRange|undefined {
-  let statementRange: StatementRange|undefined;
+        sqlDiagnosticCollection.set(document.uri, errors);
+      }
+    }
+  }
+}
+
+function shouldShowError(error: SqlSyntaxError) {
+  return error.type === `error` || (error.type === `warning` && shouldShowWarnings());
+}
+
+function getStatementRangeFromGroup(currentGroup: StatementGroup): StatementRange | undefined {
+  let statementRange: StatementRange | undefined;
   const firstStatement = currentGroup.statements[0];
   if (firstStatement && firstStatement.type !== StatementType.Unknown) {
     statementRange = [currentGroup.range.start, currentGroup.range.end];
