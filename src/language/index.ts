b4e7554b761cebdba4221d5357eb7d14b38b1847
worksofliam <mrliamallan@live.co.uk>
2025-08-20 14:00:02 -0400

    Implement command to retrieve statement groups
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/language/index.ts
+++ src/language/index.ts
@@ -1,7 +1,10 @@
+import { commands, Uri, window, workspace } from "vscode";
 import { completionProvider } from "./providers/completionProvider";
 import { formatProvider } from "./providers/formatProvider";
 import { signatureProvider } from "./providers/parameterProvider";
 import { problemProvider } from "./providers/problemProvider";
+import { getSqlDocument } from "./providers/logic/parse";
+import { StatementType } from "./sql/types";
 
 export function languageInit() {
   let functionality = [];
@@ -12,6 +15,29 @@ export function languageInit() {
     signatureProvider,
     problemProvider
   );
-  
+
+  functionality.push(...registerLanguageCommands());
+
   return functionality;
+}
+
+function registerLanguageCommands() {
+  return [
+    // Programmable API that is not callable through the UI.
+    // Solely for the use for other extensions.
+    commands.registerCommand(`vscode-db2i.language.getStatements`, async (uri?: Uri) => {
+      const document = await workspace.openTextDocument(uri);
+
+      if (document) {
+        const doc = getSqlDocument(document);
+        if (doc) {
+          const groups = doc.getStatementGroups();
+          return groups.map(g => ({
+            range: g.range,
+            type: g.statements[0] ? g.statements[0].type : StatementType.Unknown
+          }));
+        }
+      }
+    })
+  ]
 }
